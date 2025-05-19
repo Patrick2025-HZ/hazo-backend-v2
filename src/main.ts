4df6645659451ec7015f69ba15f01af6ddfa75bf
Danyal Khursheed <danyalkhursheed9@gmail.com>
2025-05-19 23:45:56 +0500

    ,,

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,6 @@ SwaggerModule.setup('api-docs', app, document);
       disableErrorMessages: false, // Show detailed error messages
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
